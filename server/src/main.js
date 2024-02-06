7e093c43fc9046e7c0e5f643f65264fb1a28f535
codewithduong05 <dbui0025@gmail.com>
2024-02-06 14:00:00 +0700

    update path deploy folder

--- server/src/main.js
+++ server/src/main.js
@@ -3,6 +3,23 @@ import app from './app.js';
 
 import dotenv from 'dotenv';
 dotenv.config();
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const frontendPath = path.join(__dirname, '../../frontend/dist');
+
+console.log("Debug !!! FE_PATH:", frontendPath);
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(frontendPath));
+
+  app.use((req, res, next) => {
+    if (req.path.startsWith('/api')) return next();
+    res.sendFile(path.join(frontendPath, 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 3000;
 
