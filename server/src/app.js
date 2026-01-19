0bbf6967dcd617e47a590cbdee8ea3a3b8691412
codewithduong05 <dbui0025@gmail.com>
2026-01-19 19:21:49 +0700

    config app js server

--- server/src/app.js
+++ server/src/app.js
@@ -1,15 +1,17 @@
 import express from 'express';
 import morgan from 'morgan';
 import bodyParser from 'body-parser';
-import routes from './routes.js';
+import db_connect from './utils/db_connect.js';
+
 
 const app = express();
 
 app.use(morgan('dev'));
 app.use(bodyParser.json());
+// console.log("hello");
 
-app.use('/api', routes);
-
+// app.use('/api', routes);
+db_connect.MONGO_DB()
 // global error handler
 app.use((err, req, res, next) => {
   console.error(err);
